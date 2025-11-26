307adeb5b8f8035562cc514035a0ccd58ea80377
Anchal kushwaha <kushwahaanchal191@gmail.com>
2025-11-26 21:04:12 +0530

    Comment code in binarySearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -28,27 +28,27 @@ public class BinarySearch {
 
 //**********************************************PEAK INDEX IN MOUNTAIN ARRAY*********************************//
 
- public static void main(String[] args) {
-       int[] arr = {2 , 3 , 4 , 5 , 6 ,4 , 3, 2 ,1}; 
-       int ans = findElem(arr);
-       System.out.println(ans);
-    }
+//  public static void main(String[] args) {
+//        int[] arr = {2 , 3 , 4 , 5 , 6 ,4 , 3, 2 ,1}; 
+//        int ans = findElem(arr);
+//        System.out.println(ans);
+//     }
 
-    static int findElem(int[] arr){
-        int start = 0;
-        int end = arr.length -1 ;
+//     static int findElem(int[] arr){
+//         int start = 0;
+//         int end = arr.length -1 ;
         
-        while(start <= end){
-            int mid = start + (end - start) /2;
+//         while(start <= end){
+//             int mid = start + (end - start) /2;
 
-            if(arr[mid] > arr[mid + 1]){
-                end = mid;
-            }else{
-                 start = mid + 1;
-            }
-        }
-        return start;
+//             if(arr[mid] > arr[mid + 1]){
+//                 end = mid;
+//             }else{
+//                  start = mid + 1;
+//             }
+//         }
+//         return start;
 
-    }
+//     }
 
     
